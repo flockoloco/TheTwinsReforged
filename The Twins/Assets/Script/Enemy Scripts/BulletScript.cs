98b32864cbd6c4436a2aa17e1949d8df15fefd5c
flockoloco <art913@hotmail.com>
2021-01-31 22:37:11 +0000

    cOMMITED

--- The Twins/Assets/Script/Enemy Scripts/BulletScript.cs
+++ The Twins/Assets/Script/Enemy Scripts/BulletScript.cs
@@ -22,26 +22,27 @@ public class BulletScript : MonoBehaviour
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(gameObject.GetComponent<Rigidbody2D>().velocity.normalized * 5, ForceMode2D.Impulse); //knocking the target towards the projectiles velocity
             Destroy(gameObject);
         }
-        if (collision.tag == "Wall")
+        if (collision.gameObject.tag == "Wall" || collision.gameObject.tag == "Door")
         {
-            if (bounces >= 1)
-            {
+            //if (bounces >= 0)
+            //{
                 bounces = bounces - 1;
                 Vector2 a = collision.ClosestPoint(gameObject.transform.position);
-                if (Mathf.Abs(a.x - gameObject.transform.position.x) >  Mathf.Abs(a.y - gameObject.transform.position.y))
+                if (Mathf.Abs(a.x - gameObject.transform.position.x) > Mathf.Abs(a.y - gameObject.transform.position.y))
                 {
                     gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(gameObject.GetComponent<Rigidbody2D>().velocity.x, gameObject.GetComponent<Rigidbody2D>().velocity.y * -1);
-                    
-                }else if (Mathf.Abs(a.x - gameObject.transform.position.x) < Mathf.Abs(a.y - gameObject.transform.position.y))
+
+                }
+                else if (Mathf.Abs(a.x - gameObject.transform.position.x) < Mathf.Abs(a.y - gameObject.transform.position.y))
                 {
                     gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(gameObject.GetComponent<Rigidbody2D>().velocity.x * -1, gameObject.GetComponent<Rigidbody2D>().velocity.y);
                 }
                 gameObject.GetComponent<Rigidbody2D>().velocity = gameObject.GetComponent<Rigidbody2D>().velocity * -1;
-            }
-            else
+            //}
+            /*if (bounces <= 0)
             {
                 Destroy(gameObject);
-            }
+            }              */
         }
-    }
+    }   
 }
